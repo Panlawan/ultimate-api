c2919368cd3ca09d07a8a696faf5d5d0d7c69de9
Panlawan <nattawat_pa62@edu.rmutl.ac.th>
2022-07-12 15:33:10 +0700

    update path of mongodb

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -9,7 +9,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   mongodb: {
-    uri: process.env.MONGO_URI || 'mongodb://nattawat_pa62:Nattawat034p@lalidts.com:27017/'
+    uri: process.env.MONGO_URI || 'mongodb://localhost/ultimate'
   },
   secret: {
     accessToken: process.env.SECRET_ACCESS_TOKEN || ''
